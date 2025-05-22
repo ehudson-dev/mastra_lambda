8f44d4c9842c815e596f008a8aa20fd8778cf466
ehudson-dev <ehudson4135@gmail.com>
2025-05-22 13:40:18 -0700

    Initial commit

---
+++ test/mastra_lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MastraLambda from '../lib/mastra_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mastra_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MastraLambda.MastraLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
